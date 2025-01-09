0ced9f9d7194c884712899486e670704e33975b0
eluza <hoggin222@Outlook.com>
2025-01-08 22:00:08 -0600

    yes

---
+++ hpcheck.lua
@@ -0,0 +1,18 @@
+-- THIS IS MAINLY FOR THE AOT:R "DEATHS DOOR" ACHIEVEMENT
+-- CAN BE USED FOR WHATEVER YOU WOULD LIKE IT FOR.
+
+local player = game.Players.LocalPlayer
+local humanoid = workspace.Characters[player.Name].Humanoid
+local gui = game.StarterGui
+
+local function notifyHealth()
+    gui:SetCore("SendNotification", {
+        Title = "HP",
+        Text = string.format("You're at %.1f%% HP", (humanoid.Health / humanoid.MaxHealth) * 100),
+        Duration = 50,
+    })
+end
+
+humanoid:GetPropertyChangedSignal("Health"):Connect(notifyHealth)
+
+notifyHealth()
